c1e90304a02a837b110134d8865879421a59d836
prateek041 <prateeksingh9741@gmail.com>
2022-09-18 11:38:38 +0530

    Done: property component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/property/PropertyList'
 import './home.css'
 
 const Home = () => {
@@ -11,6 +12,8 @@ const Home = () => {
             <div className="homeContainer">
                 <Featured />
                 <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList />
+                <h1 className="homeTitle">Homes guests love</h1>
             </div>
         </div>
     )
