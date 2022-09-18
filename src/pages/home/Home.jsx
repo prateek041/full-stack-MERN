a29358eec6b4449d23b3aa632da1fd157a27c420
prateek041 <prateeksingh9741@gmail.com>
2022-09-18 11:56:01 +0530

    Done: maillist component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/property/PropertyList'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import MailList from '../../components/mailList/MailList'
 import './home.css'
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
                 <PropertyList />
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperties />
+                <MailList />
             </div>
         </div>
     )
